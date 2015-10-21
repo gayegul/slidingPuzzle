1f0c518496e2aba7f7f0268118116349aede4cdd
Gaye Bulut <gayegul@gmail.com>
2015-10-21 09:54:48 -0700

    shuffle working

--- app/app.js
+++ app/app.js
@@ -1,35 +1,102 @@
 $(document).ready(function() {
 
-  var table = $('<table>');
-  table.appendTo($('#container'));
+  function getPuzzleReady() {
+    var table = $('<table>');
+    $('#container').append(table);
+    var numberOfRows = 4;
+    var numberOfColumns = 4;
 
-  var trNumber = 4;
-  var tdNumber = 4;
-  var dataHolder = [];
+    for(var i = 0; i < numberOfRows; i++) {
+      var tr = $('<tr>');
+      table.append(tr);
 
-  // creates a table, divides into cells and rows, assigns background for each
-  for(var j = 0; j < trNumber; j++) {
-    var tr = $('<tr>');
-    tr.appendTo(table);
-    for(var i = 0; i < tdNumber; i++) {
-      var td = $('<td>');
-      var div = $('<div>');
-      div.attr("number", i);
-      div.appendTo(td);
-      td.appendTo(tr);
-      var pos = (-157.5 * i) + 'px ' + (-88.5 * j) + 'px';
-      td.css({"background-position": pos});
+      for(var j = 0; j < numberOfColumns; j++) {
+        var div = $('<div>');
+        var width = 157.5;
+        var height = 88.5;
+        var position = (width * j * -1 + 'px ' + height * i  * -1 + 'px');
+        div.css({'background-image': 'url(http://www.personal.psu.edu/jul229/mini.jpg)', 'background-position': position});
+        div.on('click', swap);
+        var td = $('<td>');
+        td.append(div);
+        tr.append(td);
+      }
     }
-    $('#container tr:nth-child(4)').find('td:nth-child(4)').addClass('hidden');
+    $('tr:nth-child(4) td:nth-child(4) div').addClass('hidden');
   }
 
-  function move() {
-    var empty = $('tr').find('.hidden');
-    var property = $(this).css('background-position');
-    console.log(property);
-    empty.removeClass().addClass('newClass').css({"background-position": property}).show();
-    $(this).addClass('hidden');
+  function swap() {
+    var left = findLeftNeighbour($(this));
+    var right = findRightNeighbour($(this));
+    var top = findTopNeighbour($(this));
+    var bottom = findBottomNeighbour($(this));
+
+    if(left.hasClass('hidden')) {
+      swapWithEmpty($(this), left);
+    }
+    else if(right.hasClass('hidden')) {
+      swapWithEmpty($(this), right);
+    }
+    else if(top.hasClass('hidden')) {
+      swapWithEmpty($(this), top);
+    }
+    else if(bottom.hasClass('hidden')) {
+      swapWithEmpty($(this), bottom);
+    }
   }
 
-  $('td').on('click', move);
+  function swapWithEmpty(item, empty) {
+    var clickedParent = item.parent();
+    var emptyParent = empty.parent();
+
+    item.detach().appendTo(emptyParent);
+    empty.detach().appendTo(clickedParent);
+  }
+
+  function findLeftNeighbour(item) {
+    return item.parent().prev().find('div');
+  }
+  function findRightNeighbour(item) {
+    return item.parent().next().find('div');
+  }
+  function findTopNeighbour(item) {
+    var index = item.parent().index();
+    return item.parent().parent().prev().find('td:eq('+ index +')').find('div');
+  }
+  function findBottomNeighbour(item) {
+    var index = item.parent().index();
+    return item.parent().parent().next().find('td:eq('+ index +')').find('div');
+  }
+
+  function shuffleOnce() {
+    var left = findLeftNeighbour($('.hidden'));
+    var right = findRightNeighbour($('.hidden'));
+    var top = findTopNeighbour($('.hidden'));
+    var bottom = findBottomNeighbour($('.hidden'));
+    var possibleMoves = [];
+    if(left && left[0]) {
+      possibleMoves.push(left);
+    }
+    if(top && top[0]) {
+      possibleMoves.push(top);
+    }
+    if(right && right[0]) {
+      possibleMoves.push(right);
+    }
+    if(bottom && bottom[0]) {
+      possibleMoves.push(bottom);
+    }
+    var random = Math.floor(Math.random() * possibleMoves.length);
+    swapWithEmpty(possibleMoves[random], $('.hidden'));
+  }
+
+  function shuffle() {
+    for(var i = 0; i < 100; i++) {
+      shuffleOnce();
+    }
+  }
+
+  $('button').on('click', shuffle);
+  getPuzzleReady();
+
 });
